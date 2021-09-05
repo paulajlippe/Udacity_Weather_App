ac00c25df6a8c7c2f875069e99c979b6100e191a
paulajlippe <71293269+paulajlippe@users.noreply.github.com>
2021-09-05 15:23:23 -0500

    Update

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
@@ -25,8 +31,7 @@ app.use(express.static('website'));
 
 // Start up an instance of app
 
-/* Dependencies */
-/* Middleware*/
+
 
 //Here we are configuring express to use body-parser as middle-ware.
 // Cors for cross origin allowance
@@ -35,10 +40,55 @@ app.use(express.static('website'));
 
 // Spin up the server
 // Callback to debug
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`Running in localhost:${port}`)
+})
 
 // Initialize all route with a callback function
+app.post('/getWeather', async(req, res) => {
+    const {url} = req.body;
+    const response =  await fetch(`${url}${process.env.API_KEY}&units=metric`);
+    const data = await response.json();
+  
+    res.send(data);
+  })
 
 // Callback function to complete GET '/all'
 
 // Post Route
   
+
+//GET route
+app.get('/journal', getJournal);
+function getJournal(req, res){
+    let projectData = data;
+    console.log(projectData);
+    res.send(projectData);
+}
+
+//POST a journal
+const data = [];
+app.post('/journal', addJournal);
+function addJournal(req, res){
+    const newEntries = {
+        date: req.body.date,
+        location: req.body.location,
+        temp: req.body.temp,
+        weather: req.body.weather,
+        content: req.body.content
+     }
+    data.push(newEntries);
+    let projectData = data;
+    res.send(projectData);
+}
+
+//Get route for API_KEYS
+app.get('/api', getKey);
+function getKey(req, res){
+    let KEYS = {
+        "apiKey": apiKey,
+        "mapToken": mapToken
+    }
+    res.send(KEYS);
+}
